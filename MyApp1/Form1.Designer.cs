3e38c7f953358e44121077adb4ad8572fbb8ee0b
tamanna <tamanna@uni-bremen.de>
2022-04-06 21:17:17 +0200

    Initial commit

---
+++ MyApp1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace MyApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageShow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageShow
+            // 
+            this.ImageShow.Location = new System.Drawing.Point(176, 74);
+            this.ImageShow.Name = "ImageShow";
+            this.ImageShow.Size = new System.Drawing.Size(452, 325);
+            this.ImageShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageShow.TabIndex = 0;
+            this.ImageShow.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 495);
+            this.Controls.Add(this.ImageShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageShow;
+    }
+}
+
---
+++ MyApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile("C:/Users/nusra/CompARe/Screenshots/CB.jpg");
+
+            var ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            //to get the bytes
+            var bytes = ms.ToArray();
+            var imageMemoryStream = new MemoryStream(bytes);
+            Image imgFromStream = Image.FromStream(imageMemoryStream);
+            ImageShow.Image = imgFromStream;
+        }
+    }
+}
